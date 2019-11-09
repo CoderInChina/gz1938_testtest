05746132cbba4b3d985894291fbed038c7374db3
wc <wc@qq.com>
2019-11-09 10:33:57 +0800

    李四也来了

--- 01-helloworld入门/src/com/shop/aa/TestDemo2.java
+++ 01-helloworld入门/src/com/shop/aa/TestDemo2.java
@@ -5,6 +5,15 @@ public class TestDemo2 {
     public void aa1()
     {
         System.out.println("又加了一个方法...");
+
+        System.out.println("---------llalalalla");
+
+        System.out.printf("李四来了");
     }
+
+
+
+
+
 
 }
